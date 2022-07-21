abdf24da0b982d1220c7ba376a5ce1e303fd1451
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2022-07-21 08:36:10 -0300

    Adiciona exemplos com docker-compose

--- node/index.js
+++ node/index.js
@@ -1,6 +1,19 @@
+
+const mysql = require('mysql')
 const express = require('express')
 const app = express()
 const port = 3000
+const dbConfig = {
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'nodedb'
+}
+const connection = mysql.createConnection(dbConfig)
+
+const sql = `INSERT INTO people(name) VALUES('luis')`
+connection.query(sql)
+connection.end()
 
 app.get('/', (req, res) => {
     res.send('<h1>Full Cycle</h1>')
